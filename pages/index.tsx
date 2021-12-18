1af4f00d1fbb06c59fc2f517b9c596bc4f9e4c0e
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-12-18 16:04:33 +0100

    web: add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { FileDrop } from "../component/FileDrop";
-import {useCallback} from "react";
+import { FileDrop } from "../components/FileDrop";
+import { useCallback } from "react";
 
-const Home: NextPage = () => {
+export default function Home() {
   const onDrop = useCallback(async (files: File[]) => {
-    console.log('Dropped file ' + String(files))
-  }, [])
+    console.log("Dropped file " + String(files));
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-">
       <Head>
         <title>NextJS file upload with S3</title>
         <meta
@@ -19,18 +18,23 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Upload file</h1>
+      <main className="items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className={"text-2xl"}>Upload file</h1>
+        <p>File will be uploaded publicly</p>
 
-        <p className={styles.description}>uploaded file will be public</p>
-
-        <div className={styles.grid}>
-          <FileDrop onDrop={onDrop} />
+        <div className="flex items-center max-w-4xl mt-6 sm:w-full m-auto">
+          <FileDrop onDrop={onDrop} className={"w-full "} />
         </div>
       </main>
 
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center hover:underline"
+          href="https://github.com/tomfa/nextjs-s3-upload"
+        >
+          github.com/tomfa/nextjs-s3-upload
+        </a>
+      </footer>
     </div>
   );
-};
-
-export default Home;
+}
